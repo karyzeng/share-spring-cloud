c94f45c10db7255a5320f00180d7a6ce9d3bc0c4
karyzeng <354697171@qq.com>
2020-09-17 17:50:36 +0800

    spring.cloud.nacos.user.api

---
+++ spring.cloud.nacos.user.api/src/main/java/com/zzp/nacos/user/entity/User.java
@@ -0,0 +1,49 @@
+package com.zzp.nacos.user.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author zzp
+ * @since 2020-09-17
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 登录名
+     */
+    private String loginId;
+
+
+}
---
+++ spring.cloud.nacos.user.api/src/main/java/com/zzp/nacos/user/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.zzp.nacos.user.service;
+
+import com.zzp.nacos.user.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author zzp
+ * @since 2020-09-17
+ */
+public interface IUserService extends IService<User> {
+
+}
