afc0113066c0d50018db690fd6bc2370e3c56a6c
karyzeng <354697171@qq.com>
2020-09-17 17:51:52 +0800

    spring.cloud.nacos.user.provider

---
+++ spring.cloud.nacos.user.provider/src/main/java/com/zzp/nacos/user/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.zzp.nacos.user.controller;
+
+
+import com.zzp.base.results.Result;
+import com.zzp.nacos.user.entity.User;
+import com.zzp.nacos.user.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author zzp
+ * @since 2020-09-17
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value = "get", method = RequestMethod.GET)
+    public Result<User> getById(
+            @RequestParam(value = "id", required = true) Integer id
+    ) {
+        try {
+            User user = userService.getById(id);
+            return Result.ok("查询成功", user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failed("查询失败");
+        }
+    }
+
+}
