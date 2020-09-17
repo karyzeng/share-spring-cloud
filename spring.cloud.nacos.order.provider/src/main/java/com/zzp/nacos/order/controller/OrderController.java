03daca28a7549e51e0eff73e676d3be146a0adbf
zzp <354697171@qq.com>
2020-09-17 22:34:47 +0800

    spring.cloud.nacos.order.provider

---
+++ spring.cloud.nacos.order.provider/src/main/java/com/zzp/nacos/order/controller/OrderController.java
@@ -0,0 +1,46 @@
+package com.zzp.nacos.order.controller;
+
+
+import com.zzp.base.results.Result;
+import com.zzp.nacos.order.entity.Order;
+import com.zzp.nacos.order.service.IOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 订单信息 前端控制器
+ * </p>
+ *
+ * @author zzp
+ * @since 2020-09-17
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private IOrderService orderService;
+
+    @RequestMapping(value = "get", method = RequestMethod.GET)
+    public Result<Order> getById(
+            @RequestParam(value = "id", required = true) Integer id
+    ) {
+        try {
+            Order user = orderService.getById(id);
+            return Result.ok("查询成功", user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failed("查询失败");
+        }
+    }
+
+}
