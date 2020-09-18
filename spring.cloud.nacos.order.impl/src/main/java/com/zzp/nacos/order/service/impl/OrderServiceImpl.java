a3f33c1af169c79981aa6104b482c891c6d0ad0b
zzp <354697171@qq.com>
2020-09-18 22:50:40 +0800

    添加listOrders方法

--- spring.cloud.nacos.order.impl/src/main/java/com/zzp/nacos/order/service/impl/OrderServiceImpl.java
+++ spring.cloud.nacos.order.impl/src/main/java/com/zzp/nacos/order/service/impl/OrderServiceImpl.java
@@ -1,10 +1,13 @@
 package com.zzp.nacos.order.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zzp.nacos.order.entity.Order;
 import com.zzp.nacos.order.mapper.OrderMapper;
 import com.zzp.nacos.order.service.IOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements IOrderService {
 
+    @Override
+    public List<Order> listOrders(String orderNo) {
+        LambdaQueryWrapper<Order> queryWrapper = new LambdaQueryWrapper<Order>();
+        queryWrapper.eq(Order::getOrderNo, orderNo);
+        return this.list(queryWrapper);
+    }
 }
