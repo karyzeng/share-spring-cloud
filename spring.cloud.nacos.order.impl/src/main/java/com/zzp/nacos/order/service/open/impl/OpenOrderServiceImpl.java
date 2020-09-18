cf65422f93b3a49c21dead89c8b22b8a73d7bd66
zzp <354697171@qq.com>
2020-09-18 22:51:02 +0800

    order open接口实现类

--- spring.cloud.nacos.order.impl/src/main/java/com/zzp/nacos/order/service/open/impl/OpenOrderServiceImpl.java
+++ spring.cloud.nacos.order.impl/src/main/java/com/zzp/nacos/order/service/open/impl/OpenOrderServiceImpl.java
@@ -1,32 +1,38 @@
-package com.zzp.nacos.order.service.provider;
+package com.zzp.nacos.order.service.open.impl;
 
 import com.zzp.nacos.order.entity.Order;
 import com.zzp.nacos.order.service.IOrderService;
+import com.zzp.nacos.order.service.open.IOpenOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
- * @Description order服务提供者
- * @Author karyzeng
+ * @Description order open接口实现类
+ * @Author Karyzeng
  * @since 2020.09.18
  **/
 @RestController
-@RequestMapping(value = "/provider/order")
-public class OrderProvider {
+@Service
+public class OpenOrderServiceImpl implements IOpenOrderService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private IOrderService orderService;
 
-    @RequestMapping(value = "get", method = RequestMethod.GET)
+    @Override
     public Order getById(@RequestParam(value = "id", required = true) Integer id) {
         return orderService.getById(id);
     }
 
+    @Override
+    public List<Order> listOrders(String orderNo) {
+        return orderService.listOrders(orderNo);
+    }
 }
