8e772210a6485b9194ee6f008330935306244d4b
karyzeng <354697171@qq.com>
2020-09-18 16:49:00 +0800

    添加testOrder方法

--- spring.cloud.nacos.user.impl/src/main/java/com/zzp/nacos/user/service/impl/UserServiceImpl.java
+++ spring.cloud.nacos.user.impl/src/main/java/com/zzp/nacos/user/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.zzp.nacos.user.service.impl;
 
+import com.zzp.nacos.order.entity.Order;
+import com.zzp.nacos.order.service.customer.OrderCustomer;
 import com.zzp.nacos.user.entity.User;
 import com.zzp.nacos.user.mapper.UserMapper;
 import com.zzp.nacos.user.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private OrderCustomer orderCustomer;
+
+    @Override
+    public void testOrder() {
+        Order order = orderCustomer.getById(1);
+        System.out.println(order);
+    }
 }
