4b46d1298b3efd3b03c8758ab19f867f7300f76e
karyzeng <354697171@qq.com>
2020-09-18 15:02:36 +0800

    添加@EnableFeignClients注解

--- spring.cloud.nacos.order.provider/src/main/java/com/zzp/nacos/order/NacosOrderApp.java
+++ spring.cloud.nacos.order.provider/src/main/java/com/zzp/nacos/order/NacosOrderApp.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages={"com.zzp"})
 @MapperScan("com.zzp.**.mapper")
 @EnableDiscoveryClient
+@EnableFeignClients
 public class NacosOrderApp {
 
     public static void main(String[] args) {
