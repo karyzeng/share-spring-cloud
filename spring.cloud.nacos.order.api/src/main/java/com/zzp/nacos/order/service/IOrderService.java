fe44ba96a7a8e15ea2887bde3eeec787f1d586fc
zzp <354697171@qq.com>
2020-09-18 22:48:39 +0800

    添加listOrders方法

--- spring.cloud.nacos.order.api/src/main/java/com/zzp/nacos/order/service/IOrderService.java
+++ spring.cloud.nacos.order.api/src/main/java/com/zzp/nacos/order/service/IOrderService.java
@@ -3,6 +3,8 @@ package com.zzp.nacos.order.service;
 import com.zzp.nacos.order.entity.Order;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  * 订单信息 服务类
@@ -12,5 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-09-17
  */
 public interface IOrderService extends IService<Order> {
+
+    List<Order> listOrders(String orderNo);
 
 }
