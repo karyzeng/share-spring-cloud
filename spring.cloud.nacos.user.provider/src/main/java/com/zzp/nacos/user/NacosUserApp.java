dfa5204656392e4647500ffb00c68703b8e9ad77
zzp <354697171@qq.com>
2020-10-07 22:18:09 +0800

    修改NacosUserApp

--- spring.cloud.nacos.user.provider/src/main/java/com/zzp/nacos/user/NacosUserApp.java
+++ spring.cloud.nacos.user.provider/src/main/java/com/zzp/nacos/user/NacosUserApp.java
@@ -2,11 +2,17 @@ package com.zzp.nacos.user;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 /**
  * @Description 启动类
@@ -20,8 +26,23 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = {"com.zzp.nacos.*.service.open"})
 public class NacosUserApp {
 
+    @Autowired
+    private Environment env;
+
     public static void main(String[] args) {
         SpringApplication.run(NacosUserApp.class, args);
+    }
+
+    @Bean
+    public void getLocalIp(){
+        try {
+            String ipAddress = Inet4Address.getLocalHost().getHostAddress();
+            String port = env.getProperty("server.port");
+            System.out.println("localIp for Log4j2: " + ipAddress + ":" + port);
+            System.setProperty("local-ip", ipAddress + ":" + port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
 }
