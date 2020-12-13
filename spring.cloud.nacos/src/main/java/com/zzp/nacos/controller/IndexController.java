c692569fb17196becfc20fbaaf6b33ddc6da6c63
zzp <354697171@qq.com>
2020-12-13 14:38:05 +0800

    新增testAsync方法

--- spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
+++ spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.zzp.nacos.controller;
 
+import com.zzp.nacos.service.ICommonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,22 @@ public class IndexController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private ICommonService commonService;
+
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test() {
         return "hello";
+    }
+
+    @RequestMapping(value = "testAsync", method = RequestMethod.GET)
+    public String testAsync() {
+        long start = System.currentTimeMillis();
+        String name = Thread.currentThread().getName();
+        commonService.asyncTest();
+        logger.info(name + "返回了hello async");
+        long end = System.currentTimeMillis();
+        return "hello async," + (end - start);
     }
 
 }
