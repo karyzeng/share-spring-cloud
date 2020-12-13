48dfabfd1fffc2d4f5b1da127a9ac431bf227211
zzp <354697171@qq.com>
2020-12-13 14:38:31 +0800

    commonService接口

---
+++ spring.cloud.nacos/src/main/java/com/zzp/nacos/service/ICommonService.java
@@ -0,0 +1,12 @@
+package com.zzp.nacos.service;
+
+/**
+ * @Description commonService接口
+ * @Author Garyzeng
+ * @since 2020.12.12
+ **/
+public interface ICommonService {
+
+    void asyncTest();
+
+}
