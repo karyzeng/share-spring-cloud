dac1a3f7547ec4f2578e970ab00de4ef137068f1
karyzeng <354697171@qq.com>
2020-12-14 19:13:54 +0800

    公共日志实体

---
+++ spring.cloud.nacos/src/main/java/com/zzp/nacos/log/entity/CommonLogEntity.java
@@ -0,0 +1,33 @@
+package com.zzp.nacos.log.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.io.Serializable;
+
+/**
+ * @Description 公共日志实体
+ * @Author karyzeng
+ * @since 2020.12.14
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class CommonLogEntity implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Keyword)
+    private String host;
+
+    @Field(type = FieldType.Keyword)
+    private String port;
+
+}
