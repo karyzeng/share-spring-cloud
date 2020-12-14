d7214764655749c36329abb1644e6822fa51b342
karyzeng <354697171@qq.com>
2020-12-14 19:13:39 +0800

    修改testAsync方法

--- spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
+++ spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.zzp.nacos.controller;
 
+import com.zzp.nacos.log.entity.SpringCloudNacosLogEntity;
+import com.zzp.nacos.log.repository.SpringCloudNacosLogRepository;
 import com.zzp.nacos.service.ICommonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @Description nacos测试controller
@@ -22,6 +26,9 @@ public class IndexController {
     @Autowired
     private ICommonService commonService;
 
+    @Autowired
+    private SpringCloudNacosLogRepository springCloudNacosLogRepository;
+
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test() {
         return "hello";
@@ -34,6 +41,14 @@ public class IndexController {
         commonService.asyncTest();
         logger.info(name + "返回了hello async");
         long end = System.currentTimeMillis();
+
+        SpringCloudNacosLogEntity entity = new SpringCloudNacosLogEntity();
+        entity.setHost("127.0.0.1");
+        entity.setPort("8080");
+        entity.setMessage("测试直接操作ES API");
+        entity.setCreateTime(new Date());
+
+        SpringCloudNacosLogEntity entity1 = springCloudNacosLogRepository.save(entity);
         return "hello async," + (end - start);
     }
 
