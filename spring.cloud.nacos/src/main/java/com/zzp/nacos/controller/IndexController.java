8122def8a5a58673fb0afb9931df96444caa9ef2
karyzeng <354697171@qq.com>
2020-12-16 18:24:38 +0800

    修改testAsync方法

--- spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
+++ spring.cloud.nacos/src/main/java/com/zzp/nacos/controller/IndexController.java
@@ -6,10 +6,13 @@ import com.zzp.nacos.service.ICommonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 /**
@@ -22,6 +25,9 @@ import java.util.Date;
 public class IndexController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private ICommonService commonService;
@@ -43,13 +49,27 @@ public class IndexController {
         long end = System.currentTimeMillis();
 
         SpringCloudNacosLogEntity entity = new SpringCloudNacosLogEntity();
-        entity.setHost("127.0.0.1");
-        entity.setPort("8080");
+        entity.setLevel("INFO");
+        entity.setThread(Thread.currentThread().getName());
+        entity.setAppName(env.getProperty("spring.application.name"));
+        entity.setHost(this.getIp());
+        entity.setPort(env.getProperty("server.port"));
         entity.setMessage("测试直接操作ES API");
         entity.setCreateTime(new Date());
 
-        SpringCloudNacosLogEntity entity1 = springCloudNacosLogRepository.save(entity);
+        springCloudNacosLogRepository.save(entity);
         return "hello async," + (end - start);
+    }
+
+    private String getIp() {
+        String webIP = null;
+        try {
+            webIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            logger.error("获取日志Ip异常", e);
+            webIP = null;
+        }
+        return webIP;
     }
 
 }
