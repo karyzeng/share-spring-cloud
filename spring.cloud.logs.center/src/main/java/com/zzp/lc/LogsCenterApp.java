f4f856d733362d0f8dc88118ae10350b77444e1a
karyzeng <354697171@qq.com>
2020-12-16 18:49:35 +0800

    spring.cloud.logs.center

---
+++ spring.cloud.logs.center/src/main/java/com/zzp/lc/LogsCenterApp.java
@@ -0,0 +1,25 @@
+package com.zzp.lc;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Description 日志中心启动类
+ * @Author Karyzeng
+ * @since 2020.08.30
+ **/
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)// 配置多个数据源需要关闭自动配置数据源
+@ComponentScan(basePackages={"com.zzp"})
+@MapperScan("com.zzp.**.mapper")
+@EnableDiscoveryClient
+public class LogsCenterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsCenterApp.class, args);
+    }
+
+}
