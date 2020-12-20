cc9996d57cb9a18010c603d081db78d6b5331666
zzp <354697171@qq.com>
2020-12-20 15:49:48 +0800

    继承BaseLog

--- spring.cloud.logs.center.api/src/main/java/com/zzp/lc/vo/EsAppLog.java
+++ spring.cloud.logs.center.api/src/main/java/com/zzp/lc/vo/EsAppLog.java
@@ -1,14 +1,13 @@
 package com.zzp.lc.vo;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * @Description Elasticsearch App日志信息
  * @Author Garyzeng
  * @since 2020.12.19
  **/
-public class EsAppLog implements Serializable {
+public class EsAppLog extends BaseLog implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -16,26 +15,6 @@ public class EsAppLog implements Serializable {
      * Elasticsearch的索引名称
      */
     private String esIndexName;
-
-    /**
-     * 应用名称
-     */
-    private String appName;
-
-    /**
-     * 主机ip
-     */
-    private String host;
-
-    /**
-     * 端口号
-     */
-    private String port;
-
-    /**
-     * 创建时间
-     */
-    private Date createTime;
 
     public String getEsIndexName() {
         return esIndexName;
@@ -45,35 +24,4 @@ public class EsAppLog implements Serializable {
         this.esIndexName = esIndexName;
     }
 
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
