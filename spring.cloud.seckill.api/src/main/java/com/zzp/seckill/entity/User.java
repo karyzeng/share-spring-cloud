2789a1b862b984dfefed136b7e67010f0ea1d301
zzp <354697171@qq.com>
2021-01-19 22:21:08 +0800

    spring.cloud.seckill.api

---
+++ spring.cloud.seckill.api/src/main/java/com/zzp/seckill/entity/User.java
@@ -0,0 +1,9 @@
+package com.zzp.seckill.entity;
+
+/**
+ * @Description 用户信息
+ * @Author Garyzeng
+ * @since 2021.01.19
+ **/
+public class User {
+}
---
+++ spring.cloud.seckill.api/src/main/java/com/zzp/seckill/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.zzp.seckill.service;
+
+/**
+ * @Description 用户信息 service
+ * @Author Garyzeng
+ * @since 2021.01.19
+ **/
+public class IUserService {
+}
