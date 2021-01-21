aeb72c2e6a3377807fb4c57e2f7bd79e4430f17e
zzp <354697171@qq.com>
2021-01-21 22:32:32 +0800

    web mvc配置

---
+++ spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/config/WebConfigurer.java
@@ -0,0 +1,30 @@
+package com.zzp.seckill.config;
+
+import com.zzp.seckill.Interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description web mvc配置
+ * @Author Garyzeng
+ * @since 2021.01.21
+ **/
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
