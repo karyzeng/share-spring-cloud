b2812f5a8bd392b079fa8c9026605468aa08aee6
karyzeng <354697171@qq.com>
2021-01-22 17:58:58 +0800

    用户表 Mapper 接口

---
+++ spring.cloud.seckill.impl/src/main/java/com/zzp/seckill/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.zzp.seckill.mapper;
+
+import com.zzp.seckill.entity.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author zzp
+ * @since 2021-01-22
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
