8631905f3f7426ac68a5e91546c5a2a2b0675a10
karyzeng <354697171@qq.com>
2021-01-22 17:58:40 +0800

    用户表 服务类

---
+++ spring.cloud.seckill.api/src/main/java/com/zzp/seckill/service/ISysUserService.java
@@ -0,0 +1,16 @@
+package com.zzp.seckill.service;
+
+import com.zzp.seckill.entity.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author zzp
+ * @since 2021-01-22
+ */
+public interface ISysUserService extends IService<SysUser> {
+
+}
