de63c5c7d75a33b12a3824f4971f02539b4469f1
karyzeng <354697171@qq.com>
2021-01-22 17:59:16 +0800

    用户表 服务实现类

---
+++ spring.cloud.seckill.impl/src/main/java/com/zzp/seckill/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zzp.seckill.service.impl;
+
+import com.zzp.seckill.entity.SysUser;
+import com.zzp.seckill.mapper.SysUserMapper;
+import com.zzp.seckill.service.ISysUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author zzp
+ * @since 2021-01-22
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
+
+}
