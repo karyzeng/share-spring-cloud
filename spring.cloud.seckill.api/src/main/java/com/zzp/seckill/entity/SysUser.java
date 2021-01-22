fb946c7395a99b8cdd0f572693a9f653f5542b90
karyzeng <354697171@qq.com>
2021-01-22 17:58:16 +0800

    用户表

---
+++ spring.cloud.seckill.api/src/main/java/com/zzp/seckill/entity/SysUser.java
@@ -0,0 +1,80 @@
+package com.zzp.seckill.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author zzp
+ * @since 2021-01-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_sys_user")
+public class SysUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 登录账号
+     */
+    private String loginId;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 电话号码
+     */
+    private String phone;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * salt
+     */
+    private String salt;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+
+    /**
+     * 最后登录时间
+     */
+    private LocalDateTime lastLoginTime;
+
+    /**
+     * 登录次数
+     */
+    private Integer loginCount;
+
+
+}
