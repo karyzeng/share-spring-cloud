227083ce7be845fcf4d6e81aca1e64f2582a7136
karyzeng <354697171@qq.com>
2021-01-23 16:20:42 +0800

    修改preHandle方法

--- spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/Interceptor/AuthInterceptor.java
+++ spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/Interceptor/AuthInterceptor.java
@@ -1,6 +1,13 @@
 package com.zzp.seckill.Interceptor;
 
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
+import com.zzp.seckill.entity.SysUser;
+import com.zzp.seckill.redis.RedisService;
+import com.zzp.seckill.redis.UserKey;
+import com.zzp.seckill.util.CookieUtils;
 import com.zzp.seckill.util.SecurityUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,9 +24,26 @@ import java.util.UUID;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private RedisService redisService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        SecurityUtils.setUser(UUID.randomUUID().toString());
+
+        String token = request.getHeader(CookieUtils.COOKIE_NAME_TOKEN);
+        if (StringUtils.isBlank(token) && request.getParameter(CookieUtils.COOKIE_NAME_TOKEN) != null) {
+            token = request.getParameter(CookieUtils.COOKIE_NAME_TOKEN);
+        }
+
+        SysUser currentUser = redisService.get(UserKey.token, token, SysUser.class);
+        if (currentUser == null) {
+            throw new ApiException("当前登录凭证已失效");
+        }
+        // 延长redis有效期
+        redisService.set(UserKey.token, token, currentUser);
+        // 延长cookie有效期
+        CookieUtils.addCookie(response, token);
+        SecurityUtils.setUser(currentUser);
         return true;
     }
 
