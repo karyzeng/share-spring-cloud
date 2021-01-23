18fc91398b2678b7ee367b78ea4e072b3e2bd76f
karyzeng <354697171@qq.com>
2021-01-23 15:03:28 +0800

    新增login方法

--- spring.cloud.seckill.impl/src/main/java/com/zzp/seckill/service/impl/SysUserServiceImpl.java
+++ spring.cloud.seckill.impl/src/main/java/com/zzp/seckill/service/impl/SysUserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.zzp.seckill.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
 import com.zzp.seckill.entity.SysUser;
 import com.zzp.seckill.mapper.SysUserMapper;
 import com.zzp.seckill.service.ISysUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zzp.seckill.vo.LoginUserVo;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * <p>
@@ -17,4 +22,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
 
+    @Override
+    public String login(LoginUserVo loginUserVo) throws ApiException {
+        LambdaQueryWrapper<SysUser> queryWrapper = new LambdaQueryWrapper<SysUser>();
+        queryWrapper.eq(SysUser::getLoginId, loginUserVo.getLoginId());
+        queryWrapper.eq(SysUser::getPassword, loginUserVo.getPassword());
+
+        SysUser sysUser = this.getOne(queryWrapper);
+
+        if (sysUser == null) {
+            throw new ApiException("用户不存在！");
+        }
+
+        String token = UUID.randomUUID().toString().replace("-", "");
+
+        return null;
+    }
 }
