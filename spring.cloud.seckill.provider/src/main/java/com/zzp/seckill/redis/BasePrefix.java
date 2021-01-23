613cec4867eb4da9bc5ed8601cf5afa4798a4061
karyzeng <354697171@qq.com>
2021-01-23 14:49:20 +0800

    redis 基础key前缀

---
+++ spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/redis/BasePrefix.java
@@ -0,0 +1,33 @@
+package com.zzp.seckill.redis;
+
+/**
+ * redis 基础key前缀
+ * @author zzp
+ * @since 2021.01.23
+ */
+public abstract class BasePrefix implements KeyPrefix {
+
+    private int expireSeconds;
+
+    private String prefix;
+
+    public BasePrefix(String prefix){
+        this(0, prefix);//默认0代表永不过期
+    }
+
+    public BasePrefix(int expireSeconds, String prefix){
+        this.expireSeconds = expireSeconds;
+        this.prefix = prefix;
+    }
+
+    @Override
+    public int expireSeconds() {
+        return expireSeconds;
+    }
+
+    @Override
+    public String getPrefix() {
+        String className = getClass().getSimpleName();//拿到参数类类名
+        return className + ":" + prefix;
+    }
+}
