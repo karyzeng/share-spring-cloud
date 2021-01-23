6835ef1f0b910870ac0fce0ccc129b276fe3ab50
karyzeng <354697171@qq.com>
2021-01-23 16:22:04 +0800

    Userkey

---
+++ spring.cloud.seckill.impl/src/main/java/com/zzp/seckill/redis/UserKey.java
@@ -0,0 +1,25 @@
+package com.zzp.seckill.redis;
+
+/**
+ * redis Userkey
+ * @author zzp
+ * @since 2021.01.23
+ */
+public class UserKey extends BasePrefix {
+
+    public static final int TOKEN_EXPIRE = 3600*24 *2;//默认两天
+
+    /**
+     * 防止被外面实例化
+     */
+    private UserKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
+    /**
+     * 需要缓存的字段
+     */
+    public static UserKey token = new UserKey(TOKEN_EXPIRE,"token");
+    public static UserKey getById = new UserKey(0, "id");
+
+}
