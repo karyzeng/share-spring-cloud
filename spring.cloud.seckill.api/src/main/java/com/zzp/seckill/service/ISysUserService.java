1c5a1e905a28144e3a1f95438fd8f210dbcbbccb
karyzeng <354697171@qq.com>
2021-01-23 15:03:13 +0800

    新增login方法

--- spring.cloud.seckill.api/src/main/java/com/zzp/seckill/service/ISysUserService.java
+++ spring.cloud.seckill.api/src/main/java/com/zzp/seckill/service/ISysUserService.java
@@ -1,7 +1,9 @@
 package com.zzp.seckill.service;
 
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
 import com.zzp.seckill.entity.SysUser;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zzp.seckill.vo.LoginUserVo;
 
 /**
  * <p>
@@ -12,5 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-01-22
  */
 public interface ISysUserService extends IService<SysUser> {
+
+    /**
+     * 验证登录用户，如果成功则返回token
+     * @param loginUserVo 用户登录信息
+     * @return String 返回token
+     */
+    String login(LoginUserVo loginUserVo) throws ApiException;
 
 }
