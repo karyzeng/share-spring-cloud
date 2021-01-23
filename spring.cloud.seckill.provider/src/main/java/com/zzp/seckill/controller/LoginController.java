1c88a314d9d98e451c23c179d7b944a19a99a100
karyzeng <354697171@qq.com>
2021-01-23 16:20:01 +0800

    修改doLogin方法

--- spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/controller/LoginController.java
+++ spring.cloud.seckill.provider/src/main/java/com/zzp/seckill/controller/LoginController.java
@@ -1,15 +1,22 @@
 package com.zzp.seckill.controller;
 
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
 import com.zzp.base.results.Result;
+import com.zzp.seckill.service.ISysUserService;
+import com.zzp.seckill.util.CookieUtils;
 import com.zzp.seckill.vo.LoginUserVo;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Description LoginController
@@ -22,10 +29,27 @@ public class LoginController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private ISysUserService sysUserService;
+
     @RequestMapping(value = "doLogin", method = RequestMethod.POST)
     @ResponseBody
-    public Result<String> doLogin(@Validated @RequestBody LoginUserVo loginUserVo) {
-        return Result.ok("登录成功");
+    public Result<String> doLogin(HttpServletResponse response, @Validated @RequestBody LoginUserVo loginUserVo) {
+        try {
+            String token = sysUserService.login(loginUserVo);
+            if (StringUtils.isNotBlank(token)) {
+                CookieUtils.addCookie(response, token);
+                Result.ok("登录成功");
+            }
+            return Result.failed("登录失败，无法获取token");
+        } catch (ApiException e) {
+            e.printStackTrace();
+            return Result.failed(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failed("登录失败");
+        }
+
     }
 
 }
